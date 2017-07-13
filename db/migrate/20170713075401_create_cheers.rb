8e454d12d896877e438ea27cb9efce4e478ed941
ankh_user <s-nakayama@ankh-systems.co.jp>
2017-07-13 16:55:36 +0900

    migrationファイル作り直し

--- db/migrate/20170713075401_create_cheers.rb
+++ db/migrate/20170713075401_create_cheers.rb
@@ -3,8 +3,6 @@ class CreateCheers < ActiveRecord::Migration[5.1]
     create_table :cheers do |t|
       t.references :player, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
-      
-      t.string :message
 
       t.timestamps
     end
